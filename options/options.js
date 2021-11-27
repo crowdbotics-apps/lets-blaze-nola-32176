4f431ecb7798a84130598cab3009e6fa448f95cd
Crowdbotics <team@crowdbotics.com>
2021-11-27 05:55:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lets_blaze_nola_32176",
+  url: "https://lets_blaze_nola_32176.botics.co",
+  api: "https://lets_blaze_nola_32176.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
